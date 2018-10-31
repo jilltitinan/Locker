1e87cfef1962d3515715ea6ea2fde4c7cd18f15d
panisa <panisa0605@gmail.com>
2018-11-01 00:51:53 +0700

    create account table
    create account table

---
+++ Locker/Controllers/AccountController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Locker.DatabaseContext;
+using Locker.DatabaseContext.Model;
+using Locker.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Locker.Controllers
+{
+    [Route("/api/[Controller]")]
+    public class AccountController : Controller
+    {
+        private readonly AccountRepository _accountRepo;
+        private readonly LockerDbContext _dbContext;
+
+        public AccountController(LockerDbContext lockerDbContext)
+        {
+            _dbContext = lockerDbContext;
+            _accountRepo = new AccountRepository(_dbContext);
+        }
+
+        [Route("AddAccount")]
+        [HttpPost]
+        public IActionResult AddAccount([FromBody] Account account)
+        {
+            if (_accountRepo.AddAccount(account))
+            {
+                return Ok(account.Id_student);
+            }
+            return NotFound();
+        }
+
+        [Route("AddPhoneNumber")]
+        [HttpPut]
+        public IActionResult AddPhoneNumber([FromQuery] string id, string phone)
+        {
+            if (_accountRepo.AddPhoneNumber(id,phone))
+            {
+                return Ok(id);
+            }
+            return NotFound();
+        }
+
+        [Route("UpdatePoint")]
+        [HttpPut]
+        public IActionResult UpdatePoint([FromQuery] string id, int num)
+        {
+            if (_accountRepo.UpdatePoint(id,num))
+            {
+                return Ok(id);
+            }
+            return NotFound();
+        }
+
+        [Route("AccountAll")]
+        [HttpGet]
+        public IActionResult GetAccount()
+        {
+            var list = _accountRepo.GetAccount();
+            return Ok(list);
+        }
+
+        [Route("ContentId")]
+        [HttpGet]
+        public IActionResult GetAccount(string id)
+        {
+            var list = _accountRepo.GetAccount(id);
+            return Ok(list);
+        }
+    }
+}
--- Locker/Repositories/AccountRepository.cs
+++ Locker/Repositories/AccountRepository.cs
@@ -16,16 +16,69 @@ namespace Locker.Repositories
             _dbContext = dbContext;
         }
 
-        //public bool AddAccount (Account account)
-        //{
-        //    try
-        //    {
+        public bool AddAccount(Account account)
+        {
+            try
+            {
+                _dbContext.Accounts.Add(account);
+                _dbContext.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("AddAccount Error");
+                return false;
+            }
+        }
 
-        //    }
-        //    catch (Exception)
-        //    {
+        public bool AddPhoneNumber(string id,string phone)
+        {
+            try
+            {
+                if(_dbContext.Accounts.FirstOrDefault(x=>x.Id_student==id)!=null)
+                {
+                    _dbContext.Accounts.FirstOrDefault(x => x.Id_student == id).PhoneNumber = phone;
+                    _dbContext.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("AddPhoneNumber Error");
+                return false;
+            }
+        }
 
-        //    }
-        //}
+        public bool UpdatePoint (string id, int num)
+        {
+            try
+            {
+                if(_dbContext.Accounts.FirstOrDefault(x=>x.Id_student==id)!=null)
+                {
+                    _dbContext.Accounts.FirstOrDefault(x => x.Id_student == id).Point = num;
+                    _dbContext.SaveChanges();
+                    return true;
+
+                }
+                return false;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("AddPhoneNumber Error");
+                return false;
+            }
+        }
+
+        public List<Account> GetAccount()
+        {
+            return _dbContext.Accounts.ToList();
+        }
+
+        public List<Account> GetAccount(string id)
+        {
+
+            return _dbContext.Accounts.Where(x => x.Id_student == id).ToList();
+        }
     }
 }
